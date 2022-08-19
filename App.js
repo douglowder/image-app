2ae5b879ae317c6052612ac6c266c78e365a1d03
Douglas Lowder <douglowder@mac.com>
2022-08-19 13:21:38 -0700

    Local changes for douglowder

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, View, StyleSheet } from 'react-native';
+import { Image, Text, View, StyleSheet } from 'react-native';
 
-import star from './assets/star.png';
-const requireStar = require('./assets/star.png');
+// const requireStar = require('./assets/star.png');
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Image source={star} />
-			<Image source={requireStar} />
-			<StatusBar style='auto' />
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>Initial app state</Text>
+      {/*
+      <Image source={requireStar} />
+        */}
+      <StatusBar style='auto' />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
